30f738a28ff9c7a66d174f43d781d49849dd2cea
jfaulk <jamesthefaulk@gmail.com>
2014-11-23 14:37:46 -0500

    Add post_title to PostTableSeeder

--- app/database/seeds/PostTableSeeder.php
+++ app/database/seeds/PostTableSeeder.php
@@ -2,15 +2,16 @@
 
 class PostTableSeeder extends Seeder {
 
-    public function run()
-    {
-        $now = date('Y-m-d H:i:s');
+	public function run()
+	{
+		$now = date('Y-m-d H:i:s');
 
-        DB::table('posts')->insert(array(
-            'user_id' => 1,
-            'post_contents' => "<h1>Hello</h1>",
-            'created_at' => $now,
-            'updated_at' => $now,
-            ));
-    }
+		DB::table('posts')->insert(array(
+			'user_id' => 1,
+			'post_title' => 'Hello',
+			'post_contents' => "<h1>Hello</h1>",
+			'created_at' => $now,
+			'updated_at' => $now,
+			));
+	}
 }
