4c46842e5f5992562da31c63d23acce0f27a6401
William Coddington <xizor.argus@gmail.com>
2014-11-25 06:56:52 -0500

    View - blog
    initial commit of basic blog page functionality

--- app/views/blog.blade.php
+++ app/views/blog.blade.php
@@ -1,3 +1,18 @@
 @extends('master')
 
-<!-- show all posts from a given User, reverse chron order -->
+@section('header')
+	<a href="{{url('/')}}">Back to dashboard</a>
+	<h2>
+		{{{$user->display-name}}}
+	</h2>
+	<a href="{{url('users/'.$user->id.'/edit')}}">
+		<span class="glyphicon glyphicon-edit"></span> Edit
+	</a>
+	<a href="{{url('users/'.$user->id.'/delete')}}">
+		<span class="glyphicon glyphicon-trash"></span> Delete
+	</a>
+@stop
+
+@section('content')
+	{{{$user->content}}}
+@stop
