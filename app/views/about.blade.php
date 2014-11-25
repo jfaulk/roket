eb7f6b8b39fae9eeba2770b2bec3eacdb116c31b
William Coddington <xizor.argus@gmail.com>
2014-11-25 06:18:46 -0500

    about page
    tabs added for readability

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -1,7 +1,9 @@
-	@extends('master')
-	@section('header')
+@extends('master')
+
+@section('header')
 	<h2>About this site</h2>
-	@stop
-	@section('content')
+@stop
+
+@section('content')
 	<p>Poopfarts come without announcement</p>
-	@stop
+@stop
