9d58e1ca2ac5129528e016a9455ef965c4ddb9e4
William Coddington <xizor.argus@gmail.com>
2014-11-25 06:31:16 -0500

    Views
    Added files for index, post, and blog views

---
+++ app/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('master')
+
+@section('header')
+	<!-- @if user is logged in, show their username and post button -->
+	<!-- @else show signup splash screen -->
+@stop
+
+@section('content')
+	<!-- @if user is logged in, show subscribed blogs -->
+	<!-- @else hide section -->
+@stop	
