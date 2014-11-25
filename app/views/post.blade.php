1d373e9e1602b9378254662f42aef690afe9ff10
William Coddington <xizor.argus@gmail.com>
2014-11-25 06:54:34 -0500

    View - Post
    Initial commit of basic functionality

--- app/views/post.blade.php
+++ app/views/post.blade.php
@@ -1,3 +1,19 @@
 @extends('master')
 
-<!-- display one post -->
+@section('header')
+	<a href="{{url('/')}}">Back to dashboard</a>
+	<h2>
+		{{{$post->name}}}
+	</h2>
+	<a href="{{url('posts/'.$post->id.'/edit')}}">
+		<span class="glyphicon glyphicon-edit"></span> Edit
+	</a>
+	<a href="{{url('posts/'.$post->id.'/delete')}}">
+		<span class="glyphicon glyphicon-trash"></span> Delete
+	</a>
+	Last edited: {{$post->updated_at}}
+@stop
+
+@section('content')
+	{{{$post->content}}}
+@stop
