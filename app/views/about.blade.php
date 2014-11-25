6944e71cef0400cf37dfe8d353c87c34b5e596af
William Coddington <xizor.argus@gmail.com>
2014-11-25 11:48:24 -0500

    Views - About
    Populated about page with relevant info

--- app/views/about.blade.php
+++ app/views/about.blade.php
@@ -5,5 +5,29 @@
 @stop
 
 @section('content')
-	<p>Poopfarts come without announcement</p>
+	<h3>What is Roket?</h3>
+	<p>
+	    Roket is a blogging platform reimagined for the new age. It puts all content controls in the hands of the
+	    users, so users - not some impenetrable algorithm - control what they see. On Roket, you can speak, hear, and be
+	    heard by both those ready to both hear and listen. So long as you aren't doing anything illegal, you are free
+	    to speak your mind.
+	</p>
+
+	<h3>Who is Roket?</h3>
+	<p>
+	    Roket is built and maintained by three Torontonians.  They are James Faulk, Jesse Fogel, and William Coddington.
+	</p>
+
+	<h3>Why is Roket?</h3>
+	<p>
+	    Blogging sites writ large generally have pretty poor content controls afforded to users. Sure, you can block
+	    specific tags, but tags are an inexact metric. A simple box of tags doesn't differentiate between content and
+	    topic tags. Given how language is evolving at such a rapid pace in our digital age, it's difficult to maintain
+	    a standard of content tag nomenclature.
+	</p>
+	<p>
+	    Roket uses a revolutionary two-layer tagging system. By separating topic and content tags, we hope to make
+	    responsible blogging much simpler. We also aim to have the community assist site administrators in
+	    moderating content tags, so we can multiple perspectives on the same posts and accompanying tags.
+	</p>
 @stop
