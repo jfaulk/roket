f7fdd5e60a79d042c775dedcd17bcbbf913e241c
jfaulk <jamesthefaulk@gmail.com>
2014-11-26 12:40:54 -0500

    Change comment style to Blade comments {{-- --}}

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,11 +1,11 @@
 @extends('master')
 
 @section('header')
-	<!-- @if user is logged in, show their username and post button -->
-	<!-- @else show signup splash screen -->
+	{{-- @if user is logged in, show their username and post button --}}
+	{{-- @else show signup splash screen --}}
 @stop
 
 @section('content')
-	<!-- @if user is logged in, show subscribed blogs -->
-	<!-- @else hide section -->
-@stop	
+	{{-- @if user is logged in, show subscribed blogs --}}
+	{{-- @else hide section --}}
+@stop
