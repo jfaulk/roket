c017d5855af4c7ea18ad1a8cb4e102f1ef75d205
William Coddington <xizor.argus@gmail.com>
2014-11-27 20:17:16 -0500

    View - index
    - change index based on login

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -1,12 +1,17 @@
 @extends('master')
 
 @section('header')
-	{{-- @if user is logged in, show their username and post button --}}
-	{{-- @else show signup splash screen --}}
+	<h1>Roket</h1>
+	<h6>Blogging to infinity... and beyond!</h6>
 @stop
 
 @section('content')
+    @if(Auth::check()) {{-- user is logged in --}}
+    <h6>Welcome, {{{Auth::user()->username}}}</h6>
+    @else
     {{ HTML::image('img/rockt.jpg', 'a picture', array('class' => 'thumb')) }}
+    @endif
+
 	{{-- @if user is logged in, show subscribed blogs --}}
 	{{-- @else hide section --}}
 @stop
