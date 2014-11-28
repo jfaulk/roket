1ef035866566b88495510606f6b6d8ef8e69ca80
William Coddington <xizor.argus@gmail.com>
2014-11-27 22:12:57 -0500

    Post model & viewprofile view
    - implemented Post properties... hopefully
    - shows all user's posts on their viewprofile

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,6 +2,10 @@
 
 class Post extends Eloquent {
 
+    protected $table = 'posts';
+    protected $fillable = array('user_id', 'post_title', 'post_contents', 'created_at', 'updated_at');
+    protected $guarded = array('id');
+
     public function users()
     {
         return $this->belongsTo('User');
--- app/views/viewprofile.blade.php
+++ app/views/viewprofile.blade.php
@@ -2,21 +2,27 @@
 
 @section('header')
 
-{{link_to('/users', 'Back to the user list')}}
+    {{link_to('/users', 'Back to the user list')}}
+
+@stop
 
 @section('content')
+    <div>
+        <strong>ID: </strong> {{{$user->id}}} <br>
+        <strong>Name: </strong> {{{$user->name}}} <br>
+        <strong>Display Name: </strong> {{{$user->display_name}}} <br>
+        <strong>Email: </strong> {{{$user->email}}} <br>
+        <strong>Gender: </strong> {{{$user->gender}}} <br>
+        <strong>Date of Birth: </strong> {{{$user->date_of_birth}}} <br>
+        <strong>Role ID: </strong> {{{$user->role_id}}} <br>
+        <strong>Created At: </strong> {{{$user->created_at}}} <br>
+        <strong>Updated At: </strong> {{{$user->updated_at}}}
+    </div>
 
-<div>
+    <div id='blog'>
 
-<strong>ID: </strong> {{{$user->id}}} <br>
-<strong>Name: </strong> {{{$user->name}}} <br>
-<strong>Display Name: </strong> {{{$user->display_name}}} <br>
-<strong>Email: </strong> {{{$user->email}}} <br>
-<strong>Gender: </strong> {{{$user->gender}}} <br>
-<strong>Date of Birth: </strong> {{{$user->date_of_birth}}} <br>
-<strong>Role ID: </strong> {{{$user->role_id}}} <br>
-<strong>Created At: </strong> {{{$user->created_at}}} <br>
-<strong>Updated At: </strong> {{{$user->updated_at}}}
-
-</div>
+        @foreach(DB::table('posts')->where('user_id', '=', $user->id) as $barf)
+            {{{$barf->post_title . " --- " . $barf->post_contents}}}
+        @endforeach
+    </div>
 @stop
