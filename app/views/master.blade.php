317e5ec145ea300f886efe63524addf0608b5bf1
jfaulk <james.faulk@georgebrown.ca>
2014-11-29 01:09:26 -0500

    now with more validation! pretty error messages

--- app/views/master.blade.php
+++ app/views/master.blade.php
@@ -5,10 +5,10 @@
 		<meta charset="UTF-8">
 		<title>Roket</title>
 		<link rel="stylesheet" href="{{url('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css')}}">
-		{{--<link rel="stylesheet" href="{{url('https://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css')}}">--}}
+		<link rel="stylesheet" href="{{url('https://code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css')}}">
 		<link rel="stylesheet" href="{{url('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css')}}">
 		<script type="text/javascript" src="{{url('https://code.jquery.com/jquery-1.11.1.min.js')}}"></script>
-		 <script type="text/javascript" src="{{url('https://code.jquery.com/ui/1.11.2/jquery-ui.min.js')}}"></script>
+		<script type="text/javascript" src="{{url('https://code.jquery.com/ui/1.11.2/jquery-ui.min.js')}}"></script>
 		<script type="text/javascript" src="{{url('https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js')}}"></script>
 		<link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
 	</head>
