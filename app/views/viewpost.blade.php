ae0c49ab4f87279ff8bf5716b9621b205f4e227f
JesseFo <Jesse@Fogel.ws>
2014-11-30 21:41:32 -0500

    view post blade now contains content tags

--- app/views/viewpost.blade.php
+++ app/views/viewpost.blade.php
@@ -5,15 +5,22 @@
 @stop
 
 @section('content')
-    {{ "Author: " }}
-	@foreach(User::where('id', '=', $post->user_id)->get() as $auth)@endforeach
-	{{ link_to('user/'. $auth->id, $auth->name) }}
-	<br>
+    
+	Topic Tags <br>
 	{{ " | " }}
 	@foreach($post->topics as $topic)
 		{{ link_to('topic/' . $topic->id, $topic->tag)}}
 		{{ " | " }}
-	@endforeach
+	@endforeach<br><br>
+	Content Tags<br>
+	{{ " | " }}
+	@foreach($post->content as $content)
+		{{ link_to('content/' . $content->id, $content->tag)}}
+		{{ " | " }}
+	@endforeach<br>	<br>
+	{{ "Author: " }}
+	@foreach(User::where('id', '=', $post->user_id)->get() as $auth)@endforeach
+	{{ link_to('user/'. $auth->id, $auth->name) }}
 	<h1>
 		{{$post->post_title}}
 	</h1>
