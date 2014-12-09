b64fbd446834f290aa416f43e4fefd1be6ceeb79
jfaulk <james.faulk@georgebrown.ca>
2014-12-08 22:15:33 -0500

    tweak content table seeder.

--- app/database/seeds/ContentTableSeeder.php
+++ app/database/seeds/ContentTableSeeder.php
@@ -4,10 +4,28 @@ class ContentTableSeeder extends Seeder {
 	
 	public function run()
 	{
-		DB::table('content')->insert(array(
-			array('name' => 'nudity'),
-			array('name' => 'violence'),
-			array('name' => 'religion'),
-		));
+        DB::table('content')->insert(array(
+            'name' => 'nudity',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ));
+
+        DB::table('content')->insert(array(
+            'name' => 'violence',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ));
+
+        DB::table('content')->insert(array(
+            'name' => 'religion',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ));
+
+        DB::table('content')->insert(array(
+            'name' => 'none',
+            'created_at' => new DateTime,
+            'updated_at' => new DateTime,
+        ));
 	}
 }
